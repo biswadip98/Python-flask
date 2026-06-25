ca58b104efb581bf4e6d431127b43918ac28f476
Biswadip B. <86402964+biswadip98@users.noreply.github.com>
2026-06-25 08:08:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 # Demo
 from flask import Flask, render_template
 app = Flask(__name__)
-return import fafklnlask.{mf}mSL. "
-
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+return nne.
+ if 
+   go
+if go og
 
 
 @app.route('/health')
