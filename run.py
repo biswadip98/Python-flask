0066520f898af63490a270b589644b12067259a8
Python_Demo_Proect <biswadipbhattacharjee99@gmail.com>
2026-06-26 08:38:28 +0530

    Added CI

--- run.py
+++ run.py
@@ -1,2 +1,4 @@
 from app import app
+# This is a demo application for linter.
+# It is a simple Flask application to demonstrate the linter functionality.
 app.run(debug=True, host='0.0.0.0', port=80)
